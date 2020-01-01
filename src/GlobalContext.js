c4900a70f580a8294f5fe53c58f9f8a0d80c151e
rajannahar <R_N101@msn.com>
2020-01-01 13:49:16 +0000

    add try catch block

--- src/GlobalContext.js
+++ src/GlobalContext.js
@@ -2,10 +2,14 @@ import React, { createContext, useEffect, useState } from 'react';
 import axios from 'axios';
 
 const fetchPosts = async () => {
-  const url = 'https://jsonplaceholder.typicode.com/posts';
-  const result = await axios(url);
-  // console.log("result: ",result.data);
-  return result.data;
+  try {
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+    const result = await axios.get(url)
+    // console.log("result: ",result);
+    return result.data;
+  } catch (e) {
+    console.log('error ', e)
+  }
 };
 
 export const GlobalContext = createContext();
